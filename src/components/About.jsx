a74a7a7934b7e6702cf8ff8e725b4c48ae9f9ab1
GiorgioAgn <93651368+GiorgioAgn@users.noreply.github.com>
2023-03-16 16:25:31 +0100

    About - Exp - Tech - Works

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import Tilt from 'react-tilt'
-import {motion} from 'framer-motion';
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import {fadeIn,textVariant} from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({index, title, icon}) => {
-  return (
-    <Tilt className="xs:w-[250px] w-full">
-      <motion.div
-      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-      >
-        <div
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
         options={{
-          max:45,
-          scale:1,
-          speed: 450
+          max: 45,
+          scale: 1,
+          speed: 450,
         }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
-          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
-        </div>
-      </motion.div>
-    </Tilt>
-  )
-}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
@@ -39,18 +44,19 @@ const About = () => {
       </motion.div>
 
       <motion.p
-      variants={fadeIn("","",0,1,1)}
-      className="mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
         Con l'ausilio di corsi su vari settori della programmazione e linguaggi tipo Python, C, C++, Matlab, JavaScript, e frameworks come React, Node.js e Three.js sono riuscito a mettere su questo portfolio animato e interattivo. Mi credo veloce nell'imparare ed efficace nelle collaborazioni. Mi piace realizzare le idee.
       </motion.p>
+
       <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => 
+        {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
-        )}
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(About, "about") 
+export default SectionWrapper(About, "about");
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import {fadeIn, textVariant} from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+    <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Il mio lavoro</p>
+        <h2 className={styles.sectionHeadText}>Progetti</h2>
+      </motion.div>
+
+      <div className="w-full">
+
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
